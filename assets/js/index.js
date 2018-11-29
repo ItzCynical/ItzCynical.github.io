33f714bbdcf7ada2e4e79d7e717dbb51e6744a76
Xavier Chua <xchua@uci.edu>
2018-11-28 21:15:33 -0800

    Edited discord, tankjump, jetpacks/zombies/seagulls pages
    Also reorganized images into folders

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	
+
+	$('.slideshow').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		fade: true,
+		dots: true,
+		autoplay: true,
+		autoplaySpeed: 2000,
+	});
+});
